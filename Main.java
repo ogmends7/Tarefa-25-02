252e36a3dfdc96b6bda465ab6c95b9148dad1863
ogmends7 <gustavogtmendes@gmail.com>
2026-02-25 11:43:48 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Gustavo Mendes Ramalho
+    public static void main(String[] args) {
+
+        public class Exercicio1 {
+            public static void main(String[] args) {
+                int numero = 5; for (int i = 1; i <= 10; i++) {
+                    System.out.printf("%d x %d = %d%n", numero, i, numero * i);
+                }
+                }
+    }
