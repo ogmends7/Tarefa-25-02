09a9cc1b69b11c2c9d6793d8e7a2bb4fba398a95
ogmends7 <gustavogtmendes@gmail.com>
2026-02-26 09:53:50 -0300

    Add files via upload

--- Tarefa.java
+++ Tarefa.java
@@ -1,9 +1,10 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Tarefa  {
     public static void main(String[] args) {
 
-        System.out.printf("18 Anos %n1,91 Altura %nPresente");
+        System.out.printf("Gustavo Mendes Ramalho %n40028922 %nTecnólogo em Análise e Desenvolvimento de Sistemas");
 
     }
 }
