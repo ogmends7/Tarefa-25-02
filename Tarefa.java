cafcc8dbc86aeeb5a5481a006499a20537013900
ogmends7 <gustavogtmendes@gmail.com>
2026-02-26 09:52:42 -0300

    Add files via upload

---
+++ Tarefa.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Tarefa  {
+    public static void main(String[] args) {
+
+        System.out.printf("18 Anos %n1,91 Altura %nPresente");
+
+    }
+}
